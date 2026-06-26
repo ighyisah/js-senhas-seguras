388d9b548fa7587c47d964b087c062cec4383aa1
ighyisah <isadora.souza.ferreirA@escola.pr.gov.br>
2026-06-26 09:36:18 -0300

    kkk

--- README.md
+++
@@ -1,1 +0,0 @@
-# js-senhas-seguras
---
+++ main.js
@@ -0,0 +1,24 @@
+const numeroSenha = document.querySelector('.parametro-senha texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiuTamanho(){
+     if (tamanhoSenha > 1){}
+    tamanhoSenha = tamanhoSenha-1;
+     numeroSenha.textContent = tamanhoSenha;
+
+}
+
+function aumentaTamanho (){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent= tamanhoSenha;
+
+}
+      numeroSenha.textContent = tamanhoSenha;
+
+
